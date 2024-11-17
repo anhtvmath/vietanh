6490d86f7d8b6e06021d813525bf7b71cb4d78cc
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2024-11-17 17:53:48 +0700

    Update questionLC.js

--- questionLC.js
+++ questionLC.js
@@ -1,81 +1,81 @@
-document.getElementById("debaicau1").innerHTML = "Nội dung đề bài câu 1 nha"; 
+document.getElementById("debaicau1").innerHTML = "Nội dung đề bài câu 1 nha hello"; 
 document.getElementById("PAdung1").innerHTML = "Phương án đúng câu 1";
 document.getElementById("PA1Cau1").innerHTML = "Phương án sai 1 câu 1";
 document.getElementById("PA2Cau1").innerHTML = "Phương án sai 2 câu 1";
 document.getElementById("PA3Cau1").innerHTML = "Phương án sai 3 câu 1";
 document.getElementById("loigiaicau1").innerHTML = "Lời giải câu 1.<br/>";
 
-document.getElementById("debaicau2").innerHTML = "Nội dung đề bài câu 2 nha"; 
+document.getElementById("debaicau2").innerHTML = "Nội dung đề bài câu 2 nha hello"; 
 document.getElementById("PAdung2").innerHTML = "Phương án đúng câu 2";
 document.getElementById("PA1Cau2").innerHTML = "Phương án sai 1 câu 2";
 document.getElementById("PA2Cau2").innerHTML = "Phương án sai 2 câu 2";
 document.getElementById("PA3Cau2").innerHTML = "Phương án sai 3 câu 2";
 document.getElementById("loigiaicau2").innerHTML = "Lời giải câu 2.<br/>";
 
-document.getElementById("debaicau11").innerHTML = "Nội dung đề bài câu 11 nha"; 
+document.getElementById("debaicau11").innerHTML = "Nội dung đề bài câu 11 nha hello"; 
 document.getElementById("PAdung11").innerHTML = "Phương án đúng câu 11"; 
 document.getElementById("PA1Cau11").innerHTML = "Phương án sai 1 câu 11"; 
 document.getElementById("PA2Cau11").innerHTML = "Phương án sai 2 câu 11"; 
 document.getElementById("PA3Cau11").innerHTML = "Phương án sai 3 câu 11"; 
 document.getElementById("loigiaicau11").innerHTML = "Lời giải câu 11.<br/>";
 
-document.getElementById("debaicau12").innerHTML = "Nội dung đề bài câu 12 nha"; 
+document.getElementById("debaicau12").innerHTML = "Nội dung đề bài câu 12 nha hello"; 
 document.getElementById("PAdung12").innerHTML = "Phương án đúng câu 12"; 
 document.getElementById("PA1Cau12").innerHTML = "Phương án sai 1 câu 12"; 
 document.getElementById("PA2Cau12").innerHTML = "Phương án sai 2 câu 12"; 
 document.getElementById("PA3Cau12").innerHTML = "Phương án sai 3 câu 12"; 
 document.getElementById("loigiaicau12").innerHTML = "Lời giải câu 12.<br/>";
 
-document.getElementById("debaicau21").innerHTML = "Nội dung đề bài câu 21"; 
+document.getElementById("debaicau21").innerHTML = "Nội dung đề bài câu 21 nha hello"; 
 document.getElementById("PAdung21").innerHTML = "Phương án đúng câu 21"; 
 document.getElementById("PA1Cau21").innerHTML = "Phương án sai 1 câu 21"; 
 document.getElementById("PA2Cau21").innerHTML = "Phương án sai 2 câu 21"; 
 document.getElementById("PA3Cau21").innerHTML = "Phương án sai 3 câu 21"; 
 document.getElementById("loigiaicau21").innerHTML = "Lời giải câu 21.<br/>";
 
-document.getElementById("debaicau22").innerHTML = "Nội dung đề bài câu 22"; 
+document.getElementById("debaicau22").innerHTML = "Nội dung đề bài câu 22 nha hello"; 
 document.getElementById("PAdung22").innerHTML = "Phương án đúng câu 22"; 
 document.getElementById("PA1Cau22").innerHTML = "Phương án sai 1 câu 22"; 
 document.getElementById("PA2Cau22").innerHTML = "Phương án sai 2 câu 22"; 
 document.getElementById("PA3Cau22").innerHTML = "Phương án sai 3 câu 22"; 
 document.getElementById("loigiaicau22").innerHTML = "Lời giải câu 22.<br/>";
 
-document.getElementById("debaicau31").innerHTML = "Nội dung đề bài câu 31"; 
+document.getElementById("debaicau31").innerHTML = "Nội dung đề bài câu 31 nha hello"; 
 document.getElementById("PAdung31").innerHTML = "Phương án đúng câu 31"; 
 document.getElementById("PA1Cau31").innerHTML = "Phương án sai 1 câu 31"; 
 document.getElementById("PA2Cau31").innerHTML = "Phương án sai 2 câu 31"; 
 document.getElementById("PA3Cau31").innerHTML = "Phương án sai 3 câu 31"; 
 document.getElementById("loigiaicau31").innerHTML = "Lời giải câu 31.<br/>";
 
-document.getElementById("debaicau32").innerHTML = "Nội dung đề bài câu 32"; 
+document.getElementById("debaicau32").innerHTML = "Nội dung đề bài câu 32 nha hello"; 
 document.getElementById("PAdung32").innerHTML = "Phương án đúng câu 32"; 
 document.getElementById("PA1Cau32").innerHTML = "Phương án sai 1 câu 32"; 
 document.getElementById("PA2Cau32").innerHTML = "Phương án sai 2 câu 32"; 
 document.getElementById("PA3Cau32").innerHTML = "Phương án sai 3 câu 32"; 
 document.getElementById("loigiaicau32").innerHTML = "Lời giải câu 32.<br/>";
 
-document.getElementById("debaicau33").innerHTML = "Nội dung đề bài câu 33"; 
+document.getElementById("debaicau33").innerHTML = "Nội dung đề bài câu 33 nha hello"; 
 document.getElementById("PAdung33").innerHTML = "Phương án đúng câu 33"; 
 document.getElementById("PA1Cau33").innerHTML = "Phương án sai 1 câu 33"; 
 document.getElementById("PA2Cau33").innerHTML = "Phương án sai 2 câu 33"; 
 document.getElementById("PA3Cau33").innerHTML = "Phương án sai 3 câu 33"; 
 document.getElementById("loigiaicau33").innerHTML = "Lời giải câu 33.<br/>";
 
-document.getElementById("debaicau41").innerHTML = "Nội dung đề bài câu 41"; 
+document.getElementById("debaicau41").innerHTML = "Nội dung đề bài câu 41 nha hello"; 
 document.getElementById("PAdung41").innerHTML = "Phương án đúng câu 41"; 
 document.getElementById("PA1Cau41").innerHTML = "Phương án sai 1 câu 41"; 
 document.getElementById("PA2Cau41").innerHTML = "Phương án sai 2 câu 41"; 
 document.getElementById("PA3Cau41").innerHTML = "Phương án sai 3 câu 41"; 
 document.getElementById("loigiaicau41").innerHTML = "Lời giải câu 41.<br/>";
 
-document.getElementById("debaicau42").innerHTML = "Nội dung đề bài câu 42"; 
+document.getElementById("debaicau42").innerHTML = "Nội dung đề bài câu 42 nha hello"; 
 document.getElementById("PAdung42").innerHTML = "Phương án đúng câu 42"; 
 document.getElementById("PA1Cau42").innerHTML = "Phương án sai 1 câu 42"; 
 document.getElementById("PA2Cau42").innerHTML = "Phương án sai 2 câu 42"; 
 document.getElementById("PA3Cau42").innerHTML = "Phương án sai 3 câu 42"; 
 document.getElementById("loigiaicau42").innerHTML = "Lời giải câu 42.<br/>";
 
-document.getElementById("debaicau43").innerHTML = "Nội dung đề bài câu 43"; 
+document.getElementById("debaicau43").innerHTML = "Nội dung đề bài câu 43 nha hello"; 
 document.getElementById("PAdung43").innerHTML = "Phương án đúng câu 43"; 
 document.getElementById("PA1Cau43").innerHTML = "Phương án sai 1 câu 43"; 
 document.getElementById("PA2Cau43").innerHTML = "Phương án sai 2 câu 43"; 
