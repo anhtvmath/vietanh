14b61a58f6ee9fe7c84cca2caaaf298e1b961ab5
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-04-19 13:29:15 +0700

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const accounts = [
         { username: "abc", userid: "123" },
-        { username: "haidang", userid: "12345" },
+        { username: "haidang", userid: "123" },
         { username: "admin", userid: "123" },
         { username: "student123", userid: "abc123" },
         { username: "student1234", userid: "abc1234" },
