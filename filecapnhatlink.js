4c70c8ad5b27ffa2693e9e443d60310e0e26382d
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-05 15:16:34 +0700

    Update filecapnhatlink.js

--- filecapnhatlink.js
+++ filecapnhatlink.js
@@ -17,7 +17,7 @@ const headerContent = `
     </a>
 
     <a href="taocauhoi.html" style="text-decoration: none;">
-        <button type="button" style="background:#1a73e8; color:white; border:none; border-radius:4px; padding:4px 12px; cursor:pointer; font-weight:bold; font-size:12px; text-transform: uppercase; height: 16px;">
+        <button type="button" style="background:#1a73e8; color:white; border:none; border-radius:4px; padding:1px 3px; cursor:pointer; font-weight:bold; font-size:12px; text-transform: uppercase; height: 16px;">
             Tạo câu hỏi 
         </button>
     </a>
