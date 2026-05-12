f280c5188b9cf4cf63e86f723ecbbd0fc36bd9a8
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-13 06:52:14 +0700

    Add files via upload

---
+++ logintk.js
@@ -0,0 +1,6 @@
+const USERS = [
+    { user: "admin", pass: "123", name: "Quản trị viên" },
+    { user: "va", pass: "123", name: "Thầy Trần Việt Anh" },
+    { user: "hocsinh", pass: "789", name: "Em Trần Văn B" },
+    { user: "latex_pro", pass: "pro123", name: "Chuyên gia LaTeX" }
+];
