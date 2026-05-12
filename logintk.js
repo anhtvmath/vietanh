1d4e876020ef38c8b65cb6b273c31b65740fe213
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-13 06:53:03 +0700

    Update logintk.js

--- logintk.js
+++ logintk.js
@@ -1,6 +1,6 @@
 const USERS = [
     { user: "admin", pass: "123", name: "Quản trị viên" },
     { user: "va", pass: "123", name: "Thầy Trần Việt Anh" },
-    { user: "hocsinh", pass: "789", name: "Em Trần Văn B" },
+    { user: "bon", pass: "234", name: "Em Bon" },
     { user: "latex_pro", pass: "pro123", name: "Chuyên gia LaTeX" }
 ];
