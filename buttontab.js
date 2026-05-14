770fed74aa4175f8f37c78938d134b85ff506f3d
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-14 23:31:32 +0700

    Update buttontab.js

--- buttontab.js
+++ buttontab.js
@@ -97,11 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <span class="menu-label">Hình học không gian</span>
             <div class="submenu">
                 <button class="btn-math" onclick="insertAtCursor(choptamgiacdeu)">Hình chóp tam giác đều</button>
-                <button class="btn-math" onclick="insertAtCursor(chotamgiac)
-                
-                (choptamgiac)">
-                
-                Hình chóp tam giác có mặt bên vuông góc với đáy</button>
+                <button class="btn-math" onclick="insertAtCursor(choptamgiaccomatbenvuongday)">Hình chóp tam giác có mặt bên vuông góc với đáy</button>
             </div>
 
             <span class="menu-label">Tập hợp số</span>
