7f58a927741a3fe63ad6a241442e0bfaf9a9e0e2
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-14 23:05:08 +0700

    Update datatab.js

--- datatab.js
+++ datatab.js
@@ -1,3 +1,18 @@
+
+    const choptamgiacdeu = `\\begin{tikzpicture}[scale=0.6, font=\footnotesize,line join=round, line cap=round, >=stealth, every node/.style={scale=0.8}]
+\\usetikzlibrary{calc, angles, quotes}
+\\def\\ab{2} 
+\\def\\ac{4} 
+\\def\\h{3}
+\\path (0,0) coordinate (A) ++(-60:\ab) coordinate (B) (\ac,0) coordinate (C) ($(B)!1/2!(C)$) coordinate (M) ($(A)!2/3!(M)$) coordinate (H) ($(H)+(0,\h)$) coordinate (S);
+\\foreach \\i in{A,B,C}{\draw (S)--(\\i);};
+\\draw (A)--(B)--(C) (S)--(M);
+\\draw[dashed] (A)--(C) (A)--(M) (S)--(H);
+\\pic[draw,angle eccentricity=1.8,angle radius=2mm]{right angle=C--H--S};
+\\foreach \\i/\\g in {S/90,A/-180,B/-90,C/0,M/-90,H/-90} \\fill[black] (\\i) circle(1pt)+(\g:2.6mm)node[scale=1]{$\\i$};
+\end{tikzpicture}`;
+
+
     const gocbatky = `\\pic[draw, angle radius=2.5mm,"\\scriptsize $30^\\circ$"]{angle = C--B--A}`;
     const BBThambacbaCTC = `\\begin{tikzpicture}[thick,xscale=0.95,yscale=0.5]
 \\def\\a{8}
