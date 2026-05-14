88f332f8de6b9abe358adea71e57ec9da3cf237c
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-14 23:54:26 +0700

    Update datatab.js

--- datatab.js
+++ datatab.js
@@ -17,9 +17,12 @@
 \\def\\ac{5} 
 \\def\\h{4}
 \\path (0,0) coordinate (A) ++(-45:\\ab) coordinate (B) (\\ac,0) coordinate (C) ($(A)!1/2!(C)$) coordinate (H) ($(H)+(0,\\h)$) coordinate (S);
-\\foreach \\i in {A,B,C} {\draw (S)--(\\i);}
+\draw (S)--(A);
+\draw (S)--(B);
+\draw (S)--(C);
 \\draw (A)--(B)--(C);
-\\draw[dashed,thin](A)--(C) (S)--(H);
+\\draw[dashed,thin](A)--(C);
+\\draw[dashed,thin] (S)--(H);
 \\pic[draw,thin,angle radius=2mm] {right angle = S--H--C};
 \\foreach \\i/\\g in {S/90,A/180,B/-90,C/0,H/-90} \\fill[black] (\\i) circle(1pt)+(\\g:2.5mm)node[scale=1]{$\\i$};
 \\end{tikzpicture}`;
