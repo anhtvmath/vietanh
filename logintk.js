8c51ddead5fcfe49028f40ba7e40309aaa99641b
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-22 00:16:50 +0700

    Update logintk.js

--- logintk.js
+++ logintk.js
@@ -1,7 +1,7 @@
 const USERS = [
-    { user: "admin", pass: "123456789", name: "Thầy Việt Anh Toán", role: "vip"},
+    { user: "admin", pass: "123456789", name: "Thầy Việt Anh Toán"},
     { user: "va", pass: "go", name: "Thầy Trần Việt Anh", role: "vip"},
-    { user: "bon", pass: "123", name: "con Trần Nguyễn Hải Đăng" },
+    { user: "bon", pass: "123", name: "con Trần Nguyễn Hải Đăng", role: "vip"},
     { user: "0907291079", pass: "abc123", name: "Thầy Đoàn Văn Lãm" },
     { user: "loind", pass: "123", name: "Thầy Nguyễn Đức Lợi" },
     { user: "tctoan", pass: "21101994", name: "Thầy Thái Công Toãn" },
