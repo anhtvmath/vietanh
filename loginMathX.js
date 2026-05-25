5809db351d35a5bd8d626d58edccbd0967b48fd1
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-25 12:00:23 +0700

    Add files via upload

---
+++ loginMathX.js
@@ -0,0 +1,6 @@
+const USERS = [
+    { user: "admin", pass: "123456789", name: "Thầy Việt Anh Toán"},
+    { user: "va", pass: "go", name: "Thầy Trần Việt Anh"},
+    { user: "bon", pass: "123", name: "con Trần Nguyễn Hải Đăng", role: "vip"},
+    { user: "minh", pass: "123", name: "con Trần Nguyên Minh"}
+];
