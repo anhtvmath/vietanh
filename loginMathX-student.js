c15139c3c25e46979bd1b3ed2f1dd51178b6c8e6
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-30 21:31:45 +0700

    Update loginMathX-student.js

--- loginMathX-student.js
+++ loginMathX-student.js
@@ -1,5 +1,5 @@
 const USERS = [
-    { user: "bon", pass: "123", name: "con Trần Nguyễn Hải Đăng"},
-    { user: "phc", pass: "123", name: "Cô Phùng Hoàng Cúc"},
-    { user: "minh", pass: "123", name: "con Trần Nguyên Minh"}
+    {user: "embon", pass: "123", name: "con Trần Nguyễn Hải Đăng"},
+    {user: "phuc", pass: "123", name: "con Lê Thanh Phúc"},
+    {user: "emminh", pass: "123", name: "con Trần Nguyên Minh"}
 ];
