84ccfebd953a12303f0bbf8f654d6ad8897f0d38
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-05-30 16:46:22 +0700

    Update loginMathX.js

--- loginMathX.js
+++ loginMathX.js
@@ -3,6 +3,7 @@ const USERS = [
     { user: "hvt", pass: "123", name: "Thầy Hà Văn Thận"},
     { user: "loind", pass: "123", name: "Thầy Nguyễn Đức Lợi"},
     { user: "phc", pass: "123", name: "Cô Phùng Hoàng Cúc"},
+    { user: "lhl", pass: "123", name: "Thầy Lê Hoàng Lâm" },
     { user: "npt", pass: "123", name: "Thầy Nguyễn Phước Thịnh" },
     { user: "va", pass: "go", name: "Thầy Trần Việt Anh"}
 ];
