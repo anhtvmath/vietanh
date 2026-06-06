69aff54d842442347d8efe794414bdd036164bc1
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-07 00:25:19 +0700

    Add files via upload

---
+++ loginusers.js
@@ -0,0 +1,8 @@
+const USERS = [
+    {user: "minh", pass: "123", name: "Trần Nguyên Minh"},
+    {user: "bon", pass: "1", name: "Trần Nguyễn Hải Đăng"},
+    {user: "lieu", pass: "123", name: "Nguyễn Thị Liễu"},
+    {user: "hoa", pass: "12345", name: "Nguyễn Thị Hoà"},
+    {user: "quynh", pass: "12345", name: "Trần Khắc Quýnh"},
+    {user: "tva", pass: "12345", name: "Trần Việt Anh"}
+];
