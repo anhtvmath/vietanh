d15af1f7efb5a146825b866d9fe2e441a9319d9c
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-07 08:11:27 +0700

    Update loginusers.js

--- loginusers.js
+++ loginusers.js
@@ -1,8 +1,8 @@
 const USERS = [
-    {user: "minh", pass: "123", name: "Trần Nguyên Minh"},
-    {user: "bon", pass: "1", name: "Trần Nguyễn Hải Đăng"},
-    {user: "lieu", pass: "123", name: "Nguyễn Thị Liễu"},
-    {user: "hoa", pass: "12345", name: "Nguyễn Thị Hoà"},
-    {user: "quynh", pass: "12345", name: "Trần Khắc Quýnh"},
-    {user: "tva", pass: "12345", name: "Trần Việt Anh"}
+    {user: "minh", pass: "123", name: "Trần Nguyên Minh", maxAccess: 3},
+    {user: "bon", pass: "1", name: "Trần Nguyễn Hải Đăng", maxAccess: 5},
+    {user: "lieu", pass: "123", name: "Nguyễn Thị Liễu", maxAccess: 2},
+    {user: "hoa", pass: "12345", name: "Nguyễn Thị Hoà", maxAccess: 3},
+    {user: "quynh", pass: "12345", name: "Trần Khắc Quýnh", maxAccess: 3},
+    {user: "tva", pass: "12345", name: "Trần Việt Anh", maxAccess: 10}
 ];
