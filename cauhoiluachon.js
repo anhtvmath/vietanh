554d6781dd486d0face891ae25ab14374fb0e23a
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-07 08:56:43 +0700

    Update cauhoiluachon.js

--- cauhoiluachon.js
+++ cauhoiluachon.js
@@ -113,7 +113,7 @@ var ketluanArr = [" Do đó ", " Vậy "];
 var cumtu = cumtuArr[randomchoice(0, 1)];
 var ketluan = ketluanArr[randomchoice(0, 1)];
 var debai = "Cho hàm số $y=f(x)$ có bảng biến thiên như sau:\n" +
-            "\\begin{tikzpicture}[thick,xscale=0.8,yscale=0.45]\n" +
+            "\\begin{tikzpicture}[thick,xscale=0.85,yscale=0.45]\n" +
             "\\def\\a{10}\n" +
             "\\def\\b{7}\n" +
             "\\draw (-0.5,0.5) rectangle (\\a-0.5,-\\b+0.5);\n" + 
