4cd10bea6b10d2ed67b3bc1e0b7c432fab9b2952
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-09 21:01:18 +0700

    Upload diem.js

---
+++ diem.js
@@ -0,0 +1,17 @@
+function diemso(n){
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 10;
+    }
+    if (n === 2) {
+        return 25;
+    }
+    if (n === 3) {
+        return 50;
+    }
+    if (n === 4) {
+        return 100;
+    }
+}
