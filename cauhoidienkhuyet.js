8de9b295a783bb47e56e76f7ee18fdec28fcc5c6
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-10 08:20:26 +0700

    Update cauhoidienkhuyet.js

--- cauhoidienkhuyet.js
+++ cauhoidienkhuyet.js
@@ -1,1 +1,11 @@
+document.getElementById("debaicauTLN1").innerHTML = "Nội dung đề bài câu 1 nhé nhé";
+document.getElementById("keycauTLN1").innerHTML = "1,23";
+document.getElementById("loigiaicauTLN1").innerHTML = "Lời giải câu 1";
 
+document.getElementById("debaicauTLN2").innerHTML = "Nội dung đề bài câu 2 nhé nhé";
+document.getElementById("keycauTLN2").innerHTML = "2,34";
+document.getElementById("loigiaicauTLN2").innerHTML = "Lời giải câu 2";
+
+document.getElementById("debaicauTLN3").innerHTML = "Nội dung đề bài câu 3 nhé nhé";
+document.getElementById("keycauTLN3").innerHTML = "3,45";
+document.getElementById("loigiaicauTLN3").innerHTML = "Lời giải câu 3";
