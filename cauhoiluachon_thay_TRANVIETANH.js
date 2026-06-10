80f36061da9a9c9a247211f5dd1b3a5454c64269
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-11 04:14:12 +0700

    Upload cauhoiluachon_thay_TRANVIETANH.js

--- cauhoiluachon_thay_TRANVIETANH.js
+++ cauhoiluachon_thay_TRANVIETANH.js
@@ -1,13 +1,13 @@
-// Ví dụ câu 1
+// Câu 1
 diencauhoi(1, {
-    debai: "Đề bài câu 1",
-    dung: "Đáp án đúng 1",
-    sai1: "Sai 1",
-    sai2: "Sai 2",
-    sai3: "Sai 3",
+    debai: `Nghiệm của phương trình $2^{2x+1}=8$ là`,
+    dung: `$x=1$`,
+    sai1: `$x=3$`,
+    sai2: `$x=\\dfrac{3}{2}$`,
+    sai3: `$x=\\dfrac{5}{2}$`,
     loigiai: "Lời giải chi tiết câu 1."
 });
-
+// Câu 2
 diencauhoi(2, {
     debai: "Đề bài câu 2",
     dung: "Đáp án đúng 2",
@@ -16,7 +16,7 @@ diencauhoi(2, {
     sai3: "Sai 3",
     loigiai: "Lời giải chi tiết câu 2."
 });
-
+// Câu 3
 diencauhoi(3, {
     debai: "Đề bài câu 3",
     dung: "Đáp án đúng 3",
@@ -25,7 +25,7 @@ diencauhoi(3, {
     sai3: "Sai 3",
     loigiai: "Lời giải chi tiết câu 3."
 });
-
+// Câu 4
 diencauhoi(4, {
     debai: "Đề bài câu 4",
     dung: "Đáp án đúng 4",
@@ -34,7 +34,7 @@ diencauhoi(4, {
     sai3: "Sai 3",
     loigiai: "Lời giải chi tiết câu 4."
 });
-
+// Câu 5
 diencauhoi(5, {
     debai: "Đề bài câu 5",
     dung: "Đáp án đúng 5",
@@ -43,7 +43,7 @@ diencauhoi(5, {
     sai3: "Sai 3",
     loigiai: "Lời giải chi tiết câu 5."
 });
-
+// Câu 6
 diencauhoi(6, {
     debai: "Đề bài câu 6",
     dung: "Đáp án đúng 6",
