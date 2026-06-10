541c49331343edbb9a4ce4585c4f623370756cc6
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-11 04:32:27 +0700

    Upload cauhoiluachon.js

--- cauhoiluachon.js
+++ cauhoiluachon.js
@@ -153,3 +153,15 @@ document.getElementById("PA1Cau5").innerHTML = options[0];
 document.getElementById("PA2Cau5").innerHTML = options[1];
 document.getElementById("PA3Cau5").innerHTML = options[2];
 document.getElementById("loigiaicau5").innerHTML = loigiai + "<br/>";
+
+diencauhoi(6, {
+    debai: `Nghiệm của phương trình $2^{2x+1}=8$ là`,
+    dung: `$x=1$`,
+    sai1: `$x=3$`,
+    sai2: `$x=\\dfrac{3}{2}$`,
+    sai3: `$x=\\dfrac{5}{2}$`,
+    loigiai: `Ta có nhé
+    \\begin{equation*}
+    2^{2x+1}=8\\Leftrightarrow 2x+1=3\\Leftrightarrow x=1.
+    \\end{equation*}`
+});
