9ec118861e9ebb34f8e437010eda5b8bc561dc0e
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-11 03:55:44 +0700

    Update thuvienMathX.js

--- thuvienMathX.js
+++ thuvienMathX.js
@@ -1,3 +1,14 @@
+function diencauhoi(id, data) {
+    document.getElementById(`debaicau${id}`).innerHTML = data.debai;
+    document.getElementById(`PAdung${id}`).innerHTML = data.dung;
+    document.getElementById(`PA1Cau${id}`).innerHTML = data.sai1;
+    document.getElementById(`PA2Cau${id}`).innerHTML = data.sai2;
+    document.getElementById(`PA3Cau${id}`).innerHTML = data.sai3;
+    document.getElementById(`loigiaicau${id}`).innerHTML = data.loigiai + "<br/>";
+}
+
+
+
 function bangsolieu2dong(tieude1, tieude2, arrKhoang, arrTanso) {
     let html = '<table>';
     
