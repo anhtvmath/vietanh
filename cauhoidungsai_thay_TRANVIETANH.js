2d562b5fc4f07d9d8c3df606527f9f10c1bf977e
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-11 18:24:41 +0700

    Upload cauhoidungsai_thay_TRANVIETANH.js

---
+++ cauhoidungsai_thay_TRANVIETANH.js
@@ -0,0 +1,60 @@
+// Câu 1
+diencauhoiTF(1, {
+    debai: `Nội dung đề bài câu 1`,
+    Scaua: `a) Nội dung ý SAI thứ nhất`, 
+    Đcaub: `b) Nội dung ý ĐÚNG thứ hai`, 
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`, 
+    Đcaud: `d) Nội dung ý ĐÚNG thứ tư`, 
+    loigiaicaua: `a) Lời giải ý a) câu 1`,
+    loigiaicaub: `b) Lời giải ý b) câu 1`,
+    loigiaicauc: `c) Lời giải ý c) câu 1`,
+    loigiaicaud: `d) Lời giải ý d) câu 1`
+});
+// Câu 2
+diencauhoiTF(2, {
+    debai: `Nội dung đề bài câu 2`,
+    Đcaua: `a) Nội dung ý ĐÚNG thứ nhất`,
+    Scaub: `b) Nội dung ý SAI thứ hai`,
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`,
+    Scaud: `d) Nội dung ý SAI thứ tư`,
+    loigiaicaua: `a) Lời giải ý a) câu 2`,
+    loigiaicaub: `b) Lời giải ý b) câu 2`,
+    loigiaicauc: `c) Lời giải ý c) câu 2`,
+    loigiaicaud: `d) Lời giải ý d) câu 2`
+});
+// Câu 3
+diencauhoiTF(3, {
+    debai: `Nội dung đề bài câu 3`,
+    Scaua: `a) Nội dung ý SAI thứ nhất`,
+    Scaub: `b) Nội dung ý SAI thứ hai`,
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`,
+    Đcaud: `d) Nội dung ý ĐÚNG thứ tư`,
+    loigiaicaua: `a) Lời giải ý a) câu 3`,
+    loigiaicaub: `b) Lời giải ý b) câu 3`,
+    loigiaicauc: `c) Lời giải ý c) câu 3`,
+    loigiaicaud: `d) Lời giải ý d) câu 3`
+});
+// Câu 4
+diencauhoiTF(4, {
+    debai: `Nội dung đề bài câu 4`,
+    Đcaua: `a) Nội dung ý ĐÚNG thứ nhất`,
+    Đcaub: `b) Nội dung ý ĐÚNG thứ hai`,
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`,
+    Scaud: `d) Nội dung ý SAI thứ tư`,
+    loigiaicaua: `a) Lời giải ý a) câu 4`,
+    loigiaicaub: `b) Lời giải ý b) câu 4`,
+    loigiaicauc: `c) Lời giải ý c) câu 4`,
+    loigiaicaud: `d) Lời giải ý d) câu 4`
+});
+// Câu 5
+diencauhoiTF(5, {
+    debai: `Nội dung đề bài câu 5`,
+    Scaua: `a) Nội dung ý SAI thứ nhất`,
+    Đcaub: `b) Nội dung ý ĐÚNG thứ hai`,
+    Scauc: `c) Nội dung ý SAI thứ ba`,
+    Đcaud: `d) Nội dung ý ĐÚNG thứ tư`,
+    loigiaicaua: `a) Lời giải ý a) câu 5`,
+    loigiaicaub: `b) Lời giải ý b) câu 5`,
+    loigiaicauc: `c) Lời giải ý c) câu 5`,
+    loigiaicaud: `d) Lời giải ý d) câu 5`
+});
