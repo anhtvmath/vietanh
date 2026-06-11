f3c68e2550123dd18b0b0a966a5340afe6f814e5
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-11 17:14:29 +0700

    Update thoigian.js

--- thoigian.js
+++ thoigian.js
@@ -1,24 +1,3 @@
-let countdownTimer;
-
-function startTimer(name, onTimeUp) {
-    let totalSeconds = 20;
-    const timerDisplay = document.getElementById("timer");
-
-    countdownTimer = setInterval(() => {
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
-
-        timerDisplay.innerHTML = 'Chào bạn ' + name + '<br/>' +
-                        'Thời gian: ' + String(hours).padStart(2, '0') + ':' + 
-                        String(minutes).padStart(2, '0') + ':' + 
-                        String(seconds).padStart(2, '0');
-        
-        if (totalSeconds <= 0) {
-            clearInterval(countdownTimer);
-            // Gọi hàm callback khi hết giờ (ví dụ: myfunction)
-            if (onTimeUp) onTimeUp();
-        }
-        totalSeconds--;
-    }, 1000);
+function thoigian() {
+    return thoigianhambai(40);
 }
