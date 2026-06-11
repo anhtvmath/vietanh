601691d11aa6f1527423bbd211b771b3332fdd5c
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-11 23:20:50 +0700

    Upload cauhoidungsai_thay_TRANVIETANH.js

--- cauhoidungsai_thay_TRANVIETANH.js
+++ cauhoidungsai_thay_TRANVIETANH.js
@@ -1,10 +1,10 @@
 // Câu 1
 diencauhoiTF(1, {
     debai: `Nội dung đề bài câu 1`,
-    Scaua: `a) Nội dung ý SAI thứ nhất`, 
-    Đcaub: `b) Nội dung ý ĐÚNG thứ hai`, 
-    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`, 
-    Đcaud: `d) Nội dung ý ĐÚNG thứ tư`, 
+    Scaua: `a) Nội dung ý SAI thứ nhất câu 1`, 
+    Đcaub: `b) Nội dung ý ĐÚNG thứ hai câu 1`, 
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba câu 1`, 
+    Đcaud: `d) Nội dung ý ĐÚNG thứ tư câu 1`, 
     loigiaicaua: `a) Lời giải ý a) câu 1`,
     loigiaicaub: `b) Lời giải ý b) câu 1`,
     loigiaicauc: `c) Lời giải ý c) câu 1`,
@@ -13,10 +13,10 @@ diencauhoiTF(1, {
 // Câu 2
 diencauhoiTF(2, {
     debai: `Nội dung đề bài câu 2`,
-    Đcaua: `a) Nội dung ý ĐÚNG thứ nhất`,
-    Scaub: `b) Nội dung ý SAI thứ hai`,
-    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`,
-    Scaud: `d) Nội dung ý SAI thứ tư`,
+    Đcaua: `a) Nội dung ý ĐÚNG thứ nhất câu 2`,
+    Scaub: `b) Nội dung ý SAI thứ hai câu 2`,
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba câu 2`,
+    Scaud: `d) Nội dung ý SAI thứ tư câu 2`,
     loigiaicaua: `a) Lời giải ý a) câu 2`,
     loigiaicaub: `b) Lời giải ý b) câu 2`,
     loigiaicauc: `c) Lời giải ý c) câu 2`,
@@ -25,10 +25,10 @@ diencauhoiTF(2, {
 // Câu 3
 diencauhoiTF(3, {
     debai: `Nội dung đề bài câu 3`,
-    Scaua: `a) Nội dung ý SAI thứ nhất`,
-    Scaub: `b) Nội dung ý SAI thứ hai`,
-    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`,
-    Đcaud: `d) Nội dung ý ĐÚNG thứ tư`,
+    Scaua: `a) Nội dung ý SAI thứ nhất câu 3`,
+    Scaub: `b) Nội dung ý SAI thứ hai câu 3`,
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba câu 3`,
+    Đcaud: `d) Nội dung ý ĐÚNG thứ tư câu 3`,
     loigiaicaua: `a) Lời giải ý a) câu 3`,
     loigiaicaub: `b) Lời giải ý b) câu 3`,
     loigiaicauc: `c) Lời giải ý c) câu 3`,
@@ -37,10 +37,10 @@ diencauhoiTF(3, {
 // Câu 4
 diencauhoiTF(4, {
     debai: `Nội dung đề bài câu 4`,
-    Đcaua: `a) Nội dung ý ĐÚNG thứ nhất`,
-    Đcaub: `b) Nội dung ý ĐÚNG thứ hai`,
-    Đcauc: `c) Nội dung ý ĐÚNG thứ ba`,
-    Scaud: `d) Nội dung ý SAI thứ tư`,
+    Đcaua: `a) Nội dung ý ĐÚNG thứ nhất câu 4`,
+    Đcaub: `b) Nội dung ý ĐÚNG thứ hai câu 4`,
+    Đcauc: `c) Nội dung ý ĐÚNG thứ ba câu 4`,
+    Scaud: `d) Nội dung ý SAI thứ tư câu 4`,
     loigiaicaua: `a) Lời giải ý a) câu 4`,
     loigiaicaub: `b) Lời giải ý b) câu 4`,
     loigiaicauc: `c) Lời giải ý c) câu 4`,
@@ -49,10 +49,10 @@ diencauhoiTF(4, {
 // Câu 5
 diencauhoiTF(5, {
     debai: `Nội dung đề bài câu 5`,
-    Scaua: `a) Nội dung ý SAI thứ nhất`,
-    Đcaub: `b) Nội dung ý ĐÚNG thứ hai`,
-    Scauc: `c) Nội dung ý SAI thứ ba`,
-    Đcaud: `d) Nội dung ý ĐÚNG thứ tư`,
+    Scaua: `a) Nội dung ý SAI thứ nhất câu 5`,
+    Đcaub: `b) Nội dung ý ĐÚNG thứ hai câu 5`,
+    Scauc: `c) Nội dung ý SAI thứ ba câu 5`,
+    Đcaud: `d) Nội dung ý ĐÚNG thứ tư câu 5`,
     loigiaicaua: `a) Lời giải ý a) câu 5`,
     loigiaicaub: `b) Lời giải ý b) câu 5`,
     loigiaicauc: `c) Lời giải ý c) câu 5`,
