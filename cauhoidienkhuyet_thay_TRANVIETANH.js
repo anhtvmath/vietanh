74dff379a142462977d949683bf3b97e88f56137
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2026-06-12 07:40:52 +0700

    Upload cauhoidienkhuyet_thay_TRANVIETANH.js

--- cauhoidienkhuyet_thay_TRANVIETANH.js
+++ cauhoidienkhuyet_thay_TRANVIETANH.js
@@ -1,1 +1,26 @@
+// Câu 1
+diencauhoiTLN(1, {
+    debai: `Nội dung đề bài câu 1 điền khuyết`,
+    key: `dapso`,
+    loigiai: `Lời giải chi tiết câu 1.`
+});
+// Câu 2
+diencauhoiTLN(2, {
+    debai: `Nội dung đề bài câu 2 điền khuyết`,
+    key: `dapso`,
+    loigiai: `Lời giải chi tiết câu 2.`
+});
+// Câu 3
+diencauhoiTLN(3, {
+    debai: `Nội dung đề bài câu 3 điền khuyết`,
+    key: `dapso`,
+    loigiai: `Lời giải chi tiết câu 3.`
+});
+// Câu 4
+diencauhoiTLN(4, {
+    debai: `Nội dung đề bài câu 4 điền khuyết`,
+    key: `dapso`,
+    loigiai: `Lời giải chi tiết câu 4.`
+});
+
 
